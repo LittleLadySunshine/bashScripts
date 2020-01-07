e61a2d0cff32abd99182ecca0ae07078a4cfcb01
Denise McCoy <Denise.L.McCoy@gmail.com>
2020-01-07 13:36:21 -0700

    Updated to include for loop over serviceID's

--- tuningList.sh
+++ tuningList.sh
@@ -4,9 +4,20 @@ executeable="~/tuning.sh -i ${serviceID} -d ${numberofDays}d"
 echo -n "Please enter the customer id."
 read customerID
 echo ${customerID}
-fastlycli c ${customerID}
 
-curl -g -H "Fastly-Key: $Fastly_Token" "https://api.fastly.com/wafs?filter[customer_id]=${customerID}" | jq '.data | .[] | .attributes | .service_id'
+#Services with WAF for a customer.
+
+serviceIDs=$(curl -g -H "Fastly-Key: $Fastly_Token" "https://api.fastly.com/wafs?filter[customer_id]=${customerID}" | jq -r '.data | .[] | .attributes | .service_id')
+
+echo $serviceIDs
+
+#IFS=$'ln'
+for serviceID in $serviceIDs; do
+  echo $serviceID
+  fastlycli s ${serviceID}
+done
+
+exit
 
 echo -n "Which service are you looking to create a report for?"
 read serviceID
@@ -18,7 +29,3 @@ echo ${numberofDays}
 
 echo  ~/tuning.sh -i ${serviceID} -d ${numberofDays}d
 bash ~/tuning.sh -i ${serviceID} -d ${numberofDays}d
-
-
-#endpoint to list out WAF services
-#curl -g -H "Fastly-Key: $Fastly_Token" "https://api.fastly.com/wafs?filter[customer_id]=${customerID}" | jq '.data | .[] | .attributes | .service_id'
